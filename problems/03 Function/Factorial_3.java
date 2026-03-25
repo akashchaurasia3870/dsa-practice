a9d007f7f0fa761b38835de1eb35937e72aab59a
Akash Chaurasia <akash.chaurasia3870@gmail.com>
2026-03-25 09:33:21 +0530

    fact of no in java

---
+++ problems/03 Function/Factorial_3.java
@@ -0,0 +1,17 @@
+public class Factorial_3 {
+    public static long factorial(int n) {
+        if (n < 0) return -1;
+        if (n == 0 || n == 1) return 1;
+        long result = 1;
+        for (int i = 2; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(factorial(5));  // 120
+        System.out.println(factorial(0));  // 1
+        System.out.println(factorial(7));  // 5040
+    }
+}
